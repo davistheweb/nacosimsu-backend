05ece3863e3b57678899af267ff06ab0eb544bf2
davistheweb <josiahdave001@gmail.com>
2026-06-28 08:29:22 +0100

    fix backkend errors

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\File;
+
+Route::get('/where-am-i', function () {
+    return [
+        'base_path' => base_path(),
+        'storage_path' => storage_path(),
+        'public_path' => public_path(),
+        'cwd' => getcwd(),
+        'files' => File::allFiles(storage_path('app/public')),
+    ];
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
